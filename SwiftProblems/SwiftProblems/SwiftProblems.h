c80abfda4bef9a9077a5029dbe3df8b56a99b2a2
Rob Timpone <rtimpone@gmail.com>
2018-02-24 14:15:11 -0600

    Adding Xcode project

---
+++ SwiftProblems/SwiftProblems/SwiftProblems.h
@@ -0,0 +1,19 @@
+//
+//  SwiftProblems.h
+//  SwiftProblems
+//
+//  Created by Rob Timpone on 2/24/18.
+//  Copyright © 2018 Rob Timpone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftProblems.
+FOUNDATION_EXPORT double SwiftProblemsVersionNumber;
+
+//! Project version string for SwiftProblems.
+FOUNDATION_EXPORT const unsigned char SwiftProblemsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftProblems/PublicHeader.h>
+
+
